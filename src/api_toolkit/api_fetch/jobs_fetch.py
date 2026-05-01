880ba755f706ce26a92b182a05d4a4562e467b20
Manu(Manu11-Pro) <194583214+Manu11-Pro@users.noreply.github.com>
2026-05-01 13:33:26 +0530

    Refactor: correctly added page 2 and 2 scrapinga nd fetching

--- src/api_toolkit/api_fetch/jobs_fetch.py
+++ src/api_toolkit/api_fetch/jobs_fetch.py
@@ -10,42 +10,49 @@ if status_code == 200:
 
 def get_py_jobs():
     pyorg_url = "https://www.python.org/"
-    jobs_url = "https://www.python.org/jobs/?page=1" and "https://www.python.org/jobs/?page=2"
-    response = requests.get(jobs_url)
-    soup = BeautifulSoup(response.text, "html.parser")
 
-    jobs_section = soup.find("ol", class_="list-recent-jobs list-row-container menu")
+    jobs_pages = ["https://www.python.org/jobs/?page=1", "https://www.python.org/jobs/?page=2"]
 
-    if not jobs_section:
-        print("Could not find jobs section")
+    for url in jobs_pages:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    jobs_items = jobs_section.find_all("li")
+        jobs_section = soup.find("ol", class_="list-recent-jobs list-row-container menu")
 
-    blogs = []
+        if not jobs_section:
+            print("Could not find jobs section")
 
-    for item in jobs_items:
-        job_company_span = item.find("span", class_= "listing-company-name")
-        job_company_span_all_text = list(job_company_span.stripped_strings)
+        jobs_items = jobs_section.find_all("li")
 
-        job_location_span = item.find("span", class_= "listing-location")
+        jobs = []
 
-        job_type_span = item.find("span", class_= "listing-job-type")
+        for item in jobs_items:
+            job_company_span = item.find("span", class_= "listing-company-name")
+            job_company_span_all_text = list(job_company_span.stripped_strings)
 
-        job_title = item.find("a").text
-        job_company = job_company_span_all_text[-1]
-        job_partial_link = item.find("a")["href"]
-        job_full_link = f"{pyorg_url + job_partial_link}"
-        job_location = job_location_span.text.strip()
-        job_type_span_all_text = job_type_span.text.strip()
+            job_location_span = item.find("span", class_= "listing-location")
+
+            job_type_span = item.find("span", class_= "listing-job-type")
+
+            job_listing_date_span = item.find("span", class_= "listing-posted")
+
+            job_title = item.find("a").text
+            job_company = job_company_span_all_text[-1]
+            job_partial_link = item.find("a")["href"]
+            job_full_link = f"{pyorg_url + job_partial_link}"
+            job_location = job_location_span.text.strip()
+            job_type_span_all_text = job_type_span.text.strip()
+            job_listing_date_span_all_text = job_listing_date_span.text.strip()
 
 
-        print("\n")
-        print(job_title)
-        print(job_company)
-        print(job_full_link)
-        print(job_location)
-        print(job_type_span_all_text)
-        print("\n")
+            print("\n")
+            print(job_title)
+            print(job_company)
+            print(job_full_link)
+            print(job_location)
+            print(job_type_span_all_text)
+            print(job_listing_date_span_all_text)
+            print("\n")
         
 print(get_py_jobs())
 
